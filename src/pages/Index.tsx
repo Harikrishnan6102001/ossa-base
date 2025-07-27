3c25b78ca0b9535a3ca1af482529fd6a70e56143
hari <harikrish.innovation@gmail.com>
2025-07-27 15:53:10 +0530

    base

---
+++ src/pages/Index.tsx
@@ -0,0 +1,30 @@
+
+import Navigation from '../components/Navigation';
+import HeroSection from '../components/HeroSection';
+import ServicesSection from '../components/ServicesSection';
+import ProjectsSection from '../components/ProjectsSection';
+import TestimonialsSection from '../components/TestimonialsSection';
+import CustomersSection from '../components/CustomersSection';
+import AboutSection from '../components/AboutSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navigation />
+      <main className="pt-16 sm:pt-20">
+        <HeroSection />
+        <ServicesSection />
+        <ProjectsSection />
+        <TestimonialsSection />
+        <CustomersSection />
+        <AboutSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
